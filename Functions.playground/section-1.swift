dca758a2727fd863f7c889c1ef24fddf4f48f9df
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-17 23:40:24 +0900

    Add Creating a Function Step

---
+++ Functions.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Functions
+
+import UIKit
+
+println("Swift Functions")
+
+func calculateArea() {
+	let height = 12
+	let width = 10
+	let area = height * width
+	println("The area of the room is \(area)")
+}
+
+calculateArea()
