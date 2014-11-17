af9f43307ba2d44e3eecb5db978e94198adc8ef3
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-17 23:50:08 +0900

    Add Syntax and Parameters Step

--- Functions.playground/section-1.swift
+++ Functions.playground/section-1.swift
@@ -12,3 +12,16 @@ func calculateArea() {
 }
 
 calculateArea()
+
+func calculateArea(height: Int, width: Int) {
+	let area = height * width
+	println("The area of the room is \(area)")
+}
+
+calculateArea(10, 12)
+calculateArea(100, 120)
+
+// Naming Functions
+// 1. Must start with a letter and not a number or special character
+// 2. Preferably, use camel casing, such as calculateArea (where subsequent words are capitalized)
+// 3. The name must describe the purpose of the function (don't name a function a or calc which does not explain the purpose of the function, give it a meaningful name)
